ada4925fa2f6fe267ee984026386bbe3afccb63b
GouravIsCoding <130926124+GouravIsCoding@users.noreply.github.com>
2023-04-17 00:34:03 +0530

    fourth Commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
-const secret = preocess.env.SECRET || "HambaHambaHoHoHo";
+const secret = process.env.SECRET || "HambaHambaHoHoHo";
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
